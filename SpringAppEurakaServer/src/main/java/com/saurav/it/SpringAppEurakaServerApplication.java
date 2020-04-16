97d1915479c297d456c28e2424f18446b431ee2c
91797 <91797@DESKTOP-JNRVCH3>
2020-04-16 23:53:33 +0530

    hii this is first github commit

---
+++ SpringAppEurakaServer/src/main/java/com/saurav/it/SpringAppEurakaServerApplication.java
@@ -0,0 +1,14 @@
+package com.saurav.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringAppEurakaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppEurakaServerApplication.class, args);
+	}
+
+}
---
+++ SpringAppEurakaServer/src/test/java/com/saurav/it/SpringAppEurakaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurav.it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppEurakaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
